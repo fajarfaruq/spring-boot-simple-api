84829fc6ce921716955f9c8ee82d7307d16ad34b
Fajar Faruq <fajarfaruqmaulana@gmail.com>
2022-08-22 07:28:48 +0700

    :sparkles: Added Swagger API Documentation

--- src/main/java/id/my/fajarfaruq/simpleapi/SimpleApiApplication.java
+++ src/main/java/id/my/fajarfaruq/simpleapi/SimpleApiApplication.java
@@ -2,6 +2,11 @@ package id.my.fajarfaruq.simpleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Spring Boot Simple API Main Class. This class using for define main class of
@@ -10,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Fajar Faruq
  */
 @SpringBootApplication
+@EnableSwagger2
 public class SimpleApiApplication {
 
     /**
@@ -20,6 +26,12 @@ public class SimpleApiApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(SimpleApiApplication.class, args);
+    }
+    
+    @Bean
+    public Docket productApi() {
+       return new Docket(DocumentationType.SWAGGER_2).select()
+          .apis(RequestHandlerSelectors.basePackage("id.my.fajarfaruq.simpleapi.controller")).build();
     }
 
 }
